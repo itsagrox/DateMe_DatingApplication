93ff2fe79f8d2bfc87aff4b512b9008c60fa4ffc
Akash <agsworkin@gmail.com>
2021-10-07 02:49:17 +0530

    Akash || Added repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,35 +1,41 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
+using API.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-       private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly DataContext _context;
+        public readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context=context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+
         }
 
-        // [Authorize]
         [HttpGet]
-        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberModel>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();   
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
         }
 
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberModel>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
-            
+           return await _userRepository.GetMemberAsync(username);
         }
 
     }
---
+++ API/Models/PhotoModel.cs
@@ -0,0 +1,9 @@
+namespace API.Models
+{
+    public class PhotoModel
+    {
+        public int Id { get; set; }
+        public string url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
